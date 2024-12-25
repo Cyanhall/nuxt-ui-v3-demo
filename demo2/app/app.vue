87371f214e8700daf0d531dbb3e8f9618768e9d4
cyangall <lxjamsterdam@gmail.com>
2024-12-25 22:45:01 +0800

    add demo2

---
+++ demo2/app/app.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const columns = [
+  {
+    accessorKey: 'id',
+    header: 'ID'
+  },
+  {
+    accessorKey: 'name',
+    header: 'Name'
+  },
+  {
+    accessorKey: 'action',
+    header: '',
+  }
+]
+
+const data = ref<Array<any>>([
+  {id: 1, name: 'Record 1', action: 'action1'},
+  {id: 2, name: 'Record 2', action: 'action2'},
+])
+
+</script>
+
+<template>
+  <UTable
+    ref="table"
+    :columns="columns"
+    :data="data"
+    sticky
+    size="sm"
+  />
+</template>
