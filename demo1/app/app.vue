1dd22b28f2ca7f420e235d5c456c4c9804db578c
cyangall <lxjamsterdam@gmail.com>
2024-12-25 22:28:24 +0800

    add demo1

---
+++ demo1/app/app.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import type { Row } from '@tanstack/vue-table'
+import { actionColumn } from './utils/columns'
+
+const columns = [
+  {
+    accessorKey: 'id',
+    header: 'ID'
+  },
+  {
+    accessorKey: 'name',
+    header: 'Name'
+  },
+  {
+    accessorKey: 'actions',
+    header: ' ',
+    cell: ({ row }: any) => actionColumn(row, getRowItems)
+  }
+]
+
+const data = ref<Array<any>>([
+  {id: 1, name: 'Record 1'},
+  {id: 2, name: 'Record 2'},
+])
+
+
+function getRowItems(row: Row<any>) {
+  return [
+    {
+      type: 'label',
+      label: 'Actions'
+    },
+    {
+      type: 'separator'
+    }
+  ]
+}
+</script>
+
+<template>
+  <UTable
+    ref="table"
+    :columns="columns"
+    :data="data"
+    sticky
+    size="sm"
+    class="max-h-[calc(100vh-140px)]"
+  />
+</template>
---
+++ demo1/app/utils/columns.ts
@@ -0,0 +1,32 @@
+import type { Row } from '@tanstack/vue-table'
+import { UButton } from '#components'
+
+// TS Lint Error, but it works
+import { UDropdownMenu } from '#components'
+
+//  WARN  [Vue warn]: resolveComponent can only be used in render() or setup().
+// const UDropdownMenu = resolveComponent('UDropdownMenu')
+
+// 
+
+export const actionColumn = (row: Row<any>, getRowItems: (row: Row<any>) => any[]) => {
+  return h(
+    'div',
+    { class: 'text-right' },
+    h(
+      UDropdownMenu,
+      {
+        content: {
+          align: 'end'
+        },
+        items: getRowItems(row)
+      },
+      () => h(UButton, {
+        icon: 'i-lucide-ellipsis-vertical',
+        color: 'neutral',
+        variant: 'ghost',
+        class: 'ml-auto'
+      })
+    )
+  )
+}
