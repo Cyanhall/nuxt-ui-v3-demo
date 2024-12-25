db6bd125d99487099eb31ab329597408faee8524
cyangall <lxjamsterdam@gmail.com>
2024-12-26 00:02:55 +0800

    add Test component

---
+++ demo4/app/components/Test.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+const open = defineModel('open', { default: false })
+</script>
+
+<template>
+  <UButtonGroup>
+    <UButton
+      variant="outline"
+      color="neutral"
+      trailing-icon="i-lucide-pipette"
+      @click="open = true"
+    >
+     Button
+    </UButton>
+    <UModal
+      :close="true"
+      v-model:open="open"
+      title="Test"
+    >
+      <template #body>
+        Body
+      </template>
+      <template #footer>
+        <div class="flex justify-end w-full gap-4">
+          <UButton
+            variant="outline"
+            class="rounded"
+            label="Cancel"
+            @click="open = false"
+          />
+          <UButton
+            variant="outline"
+            class="rounded"
+            label="Middle"
+            @click="open = false"
+          />
+          <UButton
+            type="submit"
+            class="rounded"
+            label="Confirm"
+            color="primary"
+            @click="open = false"
+          />
+        </div>
+      </template>
+    </UModal>
+  </UButtonGroup>
+</template>
